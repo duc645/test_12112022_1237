e3a3f1539b3f05018a3439bbd0e3ef1f4f43b840
NguyenAnhDuc <anhducnguyen0703@gmail.com>
2022-11-12 13:28:03 +0700

    test push sau khi them key ssh

---
+++ test 12112022/test 12112022/Models/EF/post.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace test_12112022.Models.EF
+{
+    public class post
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string procode { get; set; }
+        public string posid { get; set; }
+    }
+}
